ddcb531d0e00c3b573e3bce0cc2057c4d58d229b
bjjeon <bjjeon@blocko.io>
2018-10-15 14:26:31 +0900

    [log] add an utility function to skip panic call stack

--- log/util.go
+++ log/util.go
@@ -8,6 +8,7 @@ package log
 import (
 	"runtime"
 	"strconv"
+	"strings"
 )
 
 // LazyEval can be used to evaluate an argument under a correct log level.
@@ -31,4 +32,19 @@ func SkipCaller(skip int) string {
 		return "?"
 	}
 	return file + ":" + strconv.Itoa(line)
+}
+
+// PanicInvoker returns caller's location (file and line) that causes panic.
+func PanicInvoker(skipTip int) string {
+	for {
+		_, file, line, ok := runtime.Caller(skipTip)
+		if !ok {
+			return "?" // unknown or an end of stack
+		} else if strings.HasSuffix(file, "runtime/panic.go") {
+			skipTip++
+			continue //skip system callstack
+		}
+
+		return file + ":" + strconv.Itoa(line)
+	}
 }
