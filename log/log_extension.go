3a4c08dcd5205202f3a819e63748b4d0c7f9162b
Hayarobi Park <hayarobipark@gmail.com>
2025-04-23 13:58:06 +0900

    LogWrapper can also be applied by caller configuration now.

--- log/log_extension.go
+++ log/log_extension.go
@@ -3,10 +3,14 @@ package log
 // WithSkipFrameCount creates new logger with sets the number of frames to skip when determining the caller information in the logger output.
 // It is useful for log wrapper.
 func (logger *Logger) WithSkipFrameCount(skipFrameCount int) *Logger {
-	zLogger := logger.With().CallerWithSkipFrameCount(skipFrameCount).Logger()
-	return &Logger{
-		Logger: &zLogger,
-		name:   logger.name,
-		level:  logger.level,
+	if enableCaller {
+		zLogger := logger.With().CallerWithSkipFrameCount(skipFrameCount).Logger()
+		return &Logger{
+			Logger: &zLogger,
+			name:   logger.name,
+			level:  logger.level,
+		}
+	} else {
+		return logger
 	}
 }
