16a9828aa40940b312fd3ccd2c66a12c5c37858b
Hayarobi Park <hayarobipark@gmail.com>
2026-07-15 18:38:11 +0900

    Fix typo
    - This patch was already applied in develop and release/1.2.1 branches.

--- db/badgerdb.go
+++ db/badgerdb.go
@@ -413,11 +413,11 @@ func newBadgerDB(dir string, opt ...Opt) (DB, error) {
 
 	// attach compaction controller with db
 	if cmpControllerEnabled {
-		logger.Info().Int("port", port).Msg("Comapaction controller enabled")
+		logger.Info().Int("port", port).Msg("Compaction controller enabled")
 		cmpController := NewCompactionController(database, port)
 		cmpController.Start()
 	} else {
-		logger.Info().Msg("Comapaction controller not enabled")
+		logger.Info().Msg("Compaction controller not enabled")
 	}
 
 	go database.runBadgerGC()
